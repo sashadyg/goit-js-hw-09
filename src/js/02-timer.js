23c2d7beda08f5697a4f469341227e461244b5d0
sashadyg <90382567+sashadyg@users.noreply.github.com>
2022-02-24 11:04:31 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const inputDataRef = document.querySelector('input')
 const options = {
@@ -10,7 +11,7 @@ const options = {
   onClose(selectedDates) { },
   onChange: function (selectedDates) {
     if (selectedDates[0].getTime() <= Date.now()) {
-      window.alert("Please choose a date in the future")
+      Notiflix.Notify.failure('Please choose a date in the future');
       startBtnRef.setAttribute('disabled', 'true')
       
     } else {
@@ -20,15 +21,15 @@ const options = {
 };
 const fp = flatpickr(inputDataRef, options)
 const startBtnRef = document.querySelector('button[data-start]')
+const daysRef = document.querySelector('span[data-days]')
+const hoursRef = document.querySelector('span[data-hours]')
+const minRef = document.querySelector('span[data-minutes]')
+const secRef = document.querySelector('span[data-seconds]')
 
-startBtnRef.addEventListener('click', startOfTimer)
-
-function startOfTimer(e) {
-  updatePerSec()
-}
+startBtnRef.addEventListener('click', updatePerSec)
 
 function updatePerSec() {
-    const timerId = setInterval(differentsOfTime, 1000);    
+  const timerId = setInterval(differentsOfTime, 1000);
 }
 
 
@@ -36,11 +37,16 @@ function differentsOfTime() {
   const selectedDate = fp.selectedDates[0].getTime()
   const nowDate = new Date()
   const gap = selectedDate - nowDate.getTime()
-  if (gap <= 0) {
-    window.alert("Please choose a date in the future")
-  }
-    
-  return gap
+  const convertedTime = convertMs(gap)
+
+  daysRef.textContent = convertedTime.days
+  hoursRef.textContent = convertedTime.hours
+  minRef.textContent = convertedTime.minutes
+  secRef.textContent = convertedTime.seconds
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0')
 }
 
 function convertMs(ms) {
@@ -49,10 +55,10 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
